8a3f2fc3329915ae2aab6a78cfb1d905f8eb94d3
Simon Valera <valmed3333@gmail.com>
2023-05-15 23:37:02 -0400

    18vo commit

---
+++ guia8-extra6/src/guia8/extra6/Guia8Extra6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia8.extra6;
+
+import Servicios.AhorcadoServicios;
+
+/**
+ *
+ * @author SimonValera
+ */
+public class Guia8Extra6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AhorcadoServicios as = new AhorcadoServicios();
+        as.juego();
+    }
+    
+}
---
+++ guia8-extra6v2/src/guia8/extra6v2/Guia8Extra6V2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia8.extra6v2;
+
+import Ahorcado.Ahorcado;
+import Servicios.ServicioJuego;
+
+public class Guia8Extra6V2 {
+
+    public static void main(String[] args) {
+        ServicioJuego SJ = new ServicioJuego();
+        Ahorcado AH = SJ.Juego();
+    }
+    
+}
